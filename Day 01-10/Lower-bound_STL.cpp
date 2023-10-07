b6d4352c36eb5d99711c927d1454f366082438d5
Kaos599 <115716485+Kaos599@users.noreply.github.com>
2023-10-07 19:53:39 +0530

    Create Lower-bound_STL.cpp

---
+++ Day 01-10/Lower-bound_STL.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int q;
+    cin >> q;
+    
+    for (int i = 0; i < q; i++) {
+        int num;
+        cin >> num;
+
+        auto it = lower_bound(arr.begin(), arr.end(), num);
+
+        if (*it == num) {
+            cout << "Yes " << (it - arr.begin() + 1) << endl;
+        } else {
+            cout << "No " << (it - arr.begin() + 1) << endl;
+        }
+    }
+
+    return 0;
+}
