a3fa53b8b5d40879d78a8dd81bd0a0dd94699a07
Harsh <115716485+Kaos599@users.noreply.github.com>
2023-11-20 22:07:24 +0530

    Update Majority element.cpp

--- Day 41-50/Majority element.cpp
+++ Day 41-50/Majority element.cpp
@@ -1,16 +1,36 @@
-🚀 Day 47/100 of #100daysofcode 🚀
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = nums[0];
+        int count = 1;
 
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == candidate) {
+                ++count;
+            } else {
+                --count;
+                if (count == 0) {
+            
+                    candidate = nums[i];
+                    count = 1;
+                }
+            }
+        }
 
+     
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                ++count;
+            }
+        }
 
-🎯 Problems Solved:-
+     
+        if (count > nums.size() / 2) {
+            return candidate;
+        }
 
-
-LeetCode: Isomorphic Strings✅
-
-
-
-Looking forward for more challenges tomorrow! 💪💻
-
-
-
-#100daysofcode #drgviswanathan #drgviswanathanchallenge #vitbhopallions #upskilling #vitbhopal #100dayscodingchallenge
+     
+        return -1;
+    }
+};
