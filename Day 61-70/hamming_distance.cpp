70767412ae363b21c8dbc8d405d1ec45817067f4
Harsh <115716485+Kaos599@users.noreply.github.com>
2023-12-06 22:21:16 +0530

    Create hamming_distance.cpp

---
+++ Day 61-70/hamming_distance.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+       
+        int xorResult = x ^ y;
+        
+
+        int count = 0;
+        while (xorResult) {
+         
+            count += xorResult & 1;
+            
+          
+            xorResult >>= 1;
+        }
+        
+        return count;
+    }
+};
