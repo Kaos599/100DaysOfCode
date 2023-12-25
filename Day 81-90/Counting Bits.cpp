b98f4e99b27ccf4b027d94c27ed88d70136091d9
Harsh <115716485+Kaos599@users.noreply.github.com>
2023-12-25 15:10:04 +0530

    Create Counting Bits.cpp

---
+++ Day 81-90/Counting Bits.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> countBits(int n) {
+        std::vector<int> result(n + 1, 0);
+
+        for (int i = 1; i <= n; ++i) {
+           
+            result[i] = result[i >> 1] + (i & 1);
+        }
+
+        return result;
+    }
+};
