aaf307155b1f9aa3aa3d797bdc2b3935259e71a5
Harsh <115716485+Kaos599@users.noreply.github.com>
2024-01-04 22:21:54 +0530

    Create Peak Element.cpp

---
+++ Day 91-100/Peak Element.cpp
@@ -0,0 +1,24 @@
+int peakElement(int arr[], int n)
+{
+   int low = 0, high = n - 1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if ((mid == 0 || arr[mid] >= arr[mid - 1]) && (mid == n - 1 || arr[mid] >= arr[mid + 1]))
+            {
+                return mid;
+            }
+            else if (mid < n - 1 && arr[mid] < arr[mid + 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+}
